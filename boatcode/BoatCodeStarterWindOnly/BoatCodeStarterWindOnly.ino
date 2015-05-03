edce2ad225f41b60707997ecd02c50827deb3e48
dianerobosail <dianemarie@alum.mit.edu>
2015-05-03 16:11:39 -0400

    Added Boat Code Starter
    Added Boat Code Starter
    Updated comments in WindSensor.ino

---
+++ boatcode/BoatCodeStarterWindOnly/BoatCodeStarterWindOnly.ino
@@ -0,0 +1,104 @@
+/* BoatCodeStarterWindOnly rev 5/2/2015
+© 2014-2015 RoboSail
+This program contains starter code for programming a boat 
+using information from the Wind Sensor.
+For detailed information about getting input from the WindSensor,
+input from the RC receiver, and sending output to the Servos,
+see these programs: WindSensor and RCPassThroughSimple
+
+Program values are displayed to the Serial Monitor when the 
+variable "verbose" is set to true.  otherwise set verbose to false.
+*/
+
+#include <Servo.h>
+boolean verbose = true;  //true calls function for values to be printed to monitor
+
+// Pin assignments
+const int ROBOSAIL_PIN_WIND = 7; 
+//input pins from receiver
+const int ROBOSAIL_PIN_RUDDER_RC = 2;
+const int ROBOSAIL_PIN_SAIL_RC = 3;
+// Output pins to the servos
+const int ROBOSAIL_PIN_RUDDER_SERVO = 8;
+const int ROBOSAIL_PIN_SAIL_SERVO = 9;
+// variables to hold input and output values
+int rudderPulseWidth;
+int rudderServoOut;
+int sailPulseWidth;
+int sailServoOut;
+//variables for WindSensor
+int windAngle = 0;
+int windPulseWidth = 0;
+
+//create servo objects
+Servo rudderServo;
+Servo sailServo;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\nRoboSail BoatCode - XXXXX");  //write program name here
+  // Set RC receiver and WindSensor on digital input pins
+  pinMode(ROBOSAIL_PIN_RUDDER_RC, INPUT);
+  pinMode(ROBOSAIL_PIN_SAIL_RC, INPUT);
+  pinMode(ROBOSAIL_PIN_WIND, INPUT);
+
+  // attach the servos to the proper pins
+  rudderServo.attach(ROBOSAIL_PIN_RUDDER_SERVO);
+  sailServo.attach(ROBOSAIL_PIN_SAIL_SERVO);
+}
+
+void loop() {
+//*********** Read in data from the RC receiver and sensors *********
+  // Read the command pulse from the RC receiver
+  rudderPulseWidth = pulseIn(ROBOSAIL_PIN_RUDDER_RC, HIGH, 25000);
+  sailPulseWidth = pulseIn(ROBOSAIL_PIN_SAIL_RC, HIGH, 25000);
+  // Calculate the servo position in degrees.
+  rudderServoOut = map(rudderPulseWidth, 1000, 2000, -75, 75);
+  sailServoOut = map(sailPulseWidth, 1090, 1900, 0, 90);
+  
+  // Read values from the WindSensor
+  windPulseWidth = pulseIn(ROBOSAIL_PIN_WIND, HIGH, 25000);
+  // Convert the wind angle to degrees from PWM.  Range -180 to +180
+  windAngle = map(windPulseWidth, 0, 1024, 180, -180);
+  windAngle = constrain(windAngle, -180, 180);
+
+//**************** your code here ******************
+// calculate values for rudderServoOut and sailServoOut in degrees.
+
+
+
+
+  if (verbose) {printToMonitor();}
+  
+//*************create output commands ***********************
+  // calculat appropriate values for the servos, then command the servos to move 
+  // the Rudder servo motor ranges from 0 to 180 with 90 deg in the center
+  // the Sailwinch servo is at ~55 deg when full-in, which we think of as 0 deg,
+  //  and ~125 deg when full out, which we think of as 90 deg
+  rudderServoOut = map(rudderServoOut, -90, 90, 0, 180);
+  rudderServo.write(rudderServoOut);
+  sailServoOut = map(sailServoOut, 0, 90, 55, 125);
+  sailServo.write(sailServoOut);
+  
+} //end of loop()
+
+
+// Function to Print out all values for debug.
+void printToMonitor()
+{
+  Serial.print("Wind Angle: ");
+  Serial.print(windAngle);
+ 
+  Serial.print("  Rudder, from RC: ");
+  Serial.print(rudderPulseWidth);
+  Serial.print("  angle out: ");
+  Serial.print(rudderServoOut);
+
+  Serial.print("\t Sail, from RC: ");
+  Serial.print(sailPulseWidth);
+  Serial.print("  angle out: ");
+  Serial.print(sailServoOut);
+
+  Serial.print("\n"); // Print a new line
+}
+
--- hardware/wind/WindSensor/WindSensor.ino
+++ hardware/wind/WindSensor/WindSensor.ino
@@ -1,7 +1,18 @@
-// Helpful program to see what the wind sensor is reading.
+/* WindSensor rev 5/2/2015
+© 2014-2015 RoboSail
+This program reads and displays data from the wind sensor, a continuous rotary 
+magnetic encoder with a 10 bit PWM output. (US Digital MA3-P10-25B)
+It is used to verify that the sensor is connected and functioning correctly
+and to calibrate the WindSensor position.
+The code reads this in using the pulseIn function then maps it to the desired range of -180 to 180 degrees. Both the pulse value and the mapped values are displayed to the serial Monitor.
 
-// This program will print the recieved values, and the corresponding angle,
-// to the serial console.
+First the PWM signal from the sensor is read in.  Nominal values for 
+the position are 0 to 1023. Use the code to determine if the actual range 
+is smaller and if there are "dead spots". You can then change the map function 
+accordingly.
+
+The input data and the corresponding angle are displayed to the serial monitor.
+*/
 
 // Pin assignments
 const int ROBOSAIL_PIN_WIND = 7; 
@@ -12,12 +23,12 @@ int windPulseWidth = 0;
 void setup() {
   Serial.begin(115200);
 
-  // Set RC receiver on digital input pins
+  // Set WindSensor on digital input pins
   pinMode(ROBOSAIL_PIN_WIND, INPUT);
 }
 
 void loop() {
-  // Read commanded (manual) values from the RC reciever
+  // Read values from the WindSensor
   // pulseIn returns the width of the command pulse in microseconds.
   windPulseWidth = pulseIn(ROBOSAIL_PIN_WIND, HIGH, 25000);
   
